b1dffc623386022555396581bd4e72ebf11fa800
MikeCoder <mkfenteng@st.ug.edu.gh>
2024-07-18 12:16:38 +0000

    backend added

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -14,4 +14,27 @@ export const GET = async () => {
 
 };
 
+export const POST = async (request: Request) => {
+  try{
+    const body = await request.json();
+    await connect();
+    const  newUser = new User(body);
+    await newUser.save();
 
+    return new NextResponse(JSON.stringify({message: "User is created", user: newUser }),{status:200}); 
+  } catch(error : any){
+    return new NextResponse("Error in creating user"+ error.message,{status:500,});
+  }
+};
+
+
+export const PATCH = async (request: Request) => {
+  try{
+    const body = await request.json();
+    const {userId, newUsername} = body;
+    await connect();
+
+  } catch(error : any){
+    return new NextResponse("Error in updating user"+ error.message,{status:500,});
+  }
+};
